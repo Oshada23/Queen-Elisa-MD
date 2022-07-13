ad23bf6f553a25ebb195b0c205e43a0dbfd260fe
Oshada23 <109217636+Oshada23@users.noreply.github.com>
2022-07-13 16:53:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,15 +32,15 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
-global.sessionName = 'session'
+global.owner = ['94767383752'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94767383752'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94767383752'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'oshada nawamina' // Your name ඔබේ නම 
+global.botnma = 'king naqiya👿💔' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'oshada' //ownernama,ownername
+global.packname = 'king nawiya Sticker' // Sticker package name 
+global.author = 'king nawiya' // Sticker Autor name 
+global.sessionName = 'osh💔'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
@@ -51,16 +51,14 @@ Download song type .yt
 Thanks fro using...`
 // Welcome massage  ඔබට කැමති නම් මෙයට ඔබෙ welcome massage  එක යොදන්න 
 global.welcome = `
-🌀 Thanks Fro using sl zim bot
-✾ Some bugs fixing
-😼♥️
+🌀 welcome 💟
 `
 // වෙල්කම් එක යවන්න ඔනි නම් add කියලා දාන්න 
 global.sendwelcome = `add`
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 king nawiya`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
